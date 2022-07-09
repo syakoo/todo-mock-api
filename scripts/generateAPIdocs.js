322340572fee18dfaaf821a9cd82b6e563488964
syakoo <syakoo.pub@gmail.com>
2022-07-09 17:58:25 +0900

    🔧 chore: udpate npm scripts.

--- scripts/generateAPIdocs.js
+++ scripts/generateAPIdocs.js
@@ -8,7 +8,7 @@ const entryPoints = {
 function main() {
   for (const entryPoint in entryPoints) {
     const inPath = path.resolve(__dirname, '../', entryPoint);
-    const outPath = path.resolve(__dirname, '../dist', entryPoints[entryPoint]);
+    const outPath = path.resolve(__dirname, '../docs', entryPoints[entryPoint]);
 
     execSync(`npx redoc-cli build ${inPath} -o ${outPath}`);
     console.log(`generate API doc: ${inPath} -> ${outPath}`);
